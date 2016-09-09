ede9cd3c2cafe0f5e06ed848559ce13c7d6a232f
Nicolas Mac Rouillon <nmr@adhoc.com.ar>
2016-09-09 11:32:01 -0300

    FIX depends for hr_sign_in_out_task module

--- hr_sign_in_out_task/hr_sign_out_project.py
+++ hr_sign_in_out_task/hr_sign_out_project.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api, _
+from openerp import models, fields, api
 
 
-class hr_sing_out_project(models.TransientModel):
+class HrSingOutProject(models.TransientModel):
 
     _inherit = 'hr.sign.out.project'
 
@@ -11,11 +11,11 @@ class hr_sing_out_project(models.TransientModel):
         'project.task',
         'Task')
     timesheet_task_required = fields.Boolean(
-        related='account_id.project_id.timesheet_task_required')
+        related='account_id.project_ids.timesheet_task_required')
 
     @api.model
     def _write(self, data, emp_id):
-        timesheet_id = super(hr_sing_out_project, self)._write(data, emp_id)
+        timesheet_id = super(HrSingOutProject, self)._write(data, emp_id)
         timesheet = self.env['hr.analytic.timesheet'].browse(timesheet_id)
         if data.task_id:
             timesheet.write({'task_id': data.task_id.id})
