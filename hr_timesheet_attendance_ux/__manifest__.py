0c0d1c15afc939d592a964feddf025b0517264b4
Joel Zilli <joz@adhoc.com.ar>
2022-12-02 09:51:08 -0300

    [FIX]hr_timesheet_attendance_ux: change domain in task_id

--- hr_timesheet_attendance_ux/__manifest__.py
+++ hr_timesheet_attendance_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Attendance Timesheet UX",
-    'version': "15.0.1.0.0",
+    'version': "15.0.1.1.0",
     'category': 'Human Resources',
     'sequence': 14,
     'author': 'ADHOC SA',
