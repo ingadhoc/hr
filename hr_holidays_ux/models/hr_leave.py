f70350fb4fbf3aabbd19c42755dbd6625e8181ce
Virginia <vib@adhoc.com.ar>
2025-01-16 11:34:08 -0300

    [IMP] *: pre-commit automatic fixes

--- hr_holidays_ux/models/hr_leave.py
+++ hr_holidays_ux/models/hr_leave.py
@@ -1,9 +1,11 @@
-from odoo import api, fields, models, _
-from datetime import timedelta
 import calendar
+from datetime import timedelta
+
+from odoo import api, fields, models
+
 
 class HrLeave(models.Model):
-    _inherit = 'hr.leave'
+    _inherit = "hr.leave"
 
     def get_last_day_of_month(self, date):
         """Get last day of the month from the given date using monthrange."""
@@ -13,50 +15,55 @@ class HrLeave(models.Model):
     def split_days(self, rec):
         """Split leave days across months."""
         new_records = []
-        date_from = rec['request_date_from']
-        original_date_to = rec['request_date_to']
+        date_from = rec["request_date_from"]
+        original_date_to = rec["request_date_to"]
 
         while date_from <= original_date_to:
             # get last day of the month
             end_of_month = self.get_last_day_of_month(date_from)
             if original_date_to <= end_of_month:
                 # save the dates of the single record to be created
-                new_records.append({
-                    'request_date_from': date_from,
-                    'request_date_to': original_date_to,
-                })
+                new_records.append(
+                    {
+                        "request_date_from": date_from,
+                        "request_date_to": original_date_to,
+                    }
+                )
                 break
             else:
                 # save the dates of each record to be created until reaching the end date of the leave, original_date_to
-                new_records.append({ 
-                    'request_date_from': date_from,
-                    'request_date_to': end_of_month,
-                })
+                new_records.append(
+                    {
+                        "request_date_from": date_from,
+                        "request_date_to": end_of_month,
+                    }
+                )
                 date_from = end_of_month + timedelta(days=1)
 
         return new_records
 
-    
     @api.model_create_multi
     def create(self, vals_list):
         new_vals_list = []
         for vals in vals_list:
-            date_from = vals.get('request_date_from')
-            date_to = vals.get('request_date_to')
+            date_from = vals.get("request_date_from")
+            date_to = vals.get("request_date_to")
 
             if date_from and date_to:
                 # convert to datetime objects if they are strings
                 if isinstance(date_from, str):
                     date_from = fields.Datetime.from_string(date_from)
                 if isinstance(date_to, str):
-                    date_to = fields.Datetime.from_string(date_to)                
-            
+                    date_to = fields.Datetime.from_string(date_to)
+
             # if the leave ends in a different month, call the split_days() method to get the dates for splitting the leaves
             if date_to > self.get_last_day_of_month(date_from):
-                split_records = self.split_days({
-                    'request_date_from' : date_from,
-                    'request_date_to' : date_to,
-                })
+                split_records = self.split_days(
+                    {
+                        "request_date_from": date_from,
+                        "request_date_to": date_to,
+                    }
+                )
                 # for each record generated in split_days, copy the original vals, and update with the dates of the record rec, adding them to a new list
                 for rec in split_records:
                     new_vals = vals.copy()
@@ -66,4 +73,3 @@ class HrLeave(models.Model):
                 new_vals_list.append(vals)
 
         return super().create(new_vals_list)
-    
--- hr_timesheet_attendance_ux/models/account_analytic_line.py
+++ hr_timesheet_attendance_ux/models/account_analytic_line.py
@@ -1,38 +1,36 @@
 from datetime import timedelta
-from odoo import models, api, fields
+
+from odoo import api, fields, models
 
 
 class AccountAnalyticLine(models.Model):
-
     _inherit = "account.analytic.line"
 
     task_id = fields.Many2one(
-        domain="[('company_id', '=', company_id), ('project_id.allow_timesheets', '=', True), ('project_id', '=?', project_id), ('stage_id.fold', '=', False)]")
+        domain="[('company_id', '=', company_id), ('project_id.allow_timesheets', '=', True), ('project_id', '=?', project_id), ('stage_id.fold', '=', False)]"
+    )
 
-    @api.onchange('employee_id', 'date')
+    @api.onchange("employee_id", "date")
     def onchange_compute_hours(self):
-        user_employee = self.default_get(
-            ['employee_id', 'user_id']).get('employee_id')
-        hr_attendance = self.env['hr.attendance']
+        user_employee = self.default_get(["employee_id", "user_id"]).get("employee_id")
+        hr_attendance = self.env["hr.attendance"]
         for rec in self:
             end = rec.date + timedelta(hours=23, minutes=59, seconds=59)
             employee_id = rec.employee_id.id or user_employee
-            attendances = hr_attendance.search([
-                ('employee_id', '=', employee_id),
-                ('check_in', '>=', rec.date),
-                ('check_in', '<=', end),
-            ])
-            domain = [
-                ('employee_id', '=', employee_id),
-                ('date', '=', rec.date)]
-            if 'holiday_id' in self._fields:
-                domain.append(('holiday_id', '=', None))
+            attendances = hr_attendance.search(
+                [
+                    ("employee_id", "=", employee_id),
+                    ("check_in", ">=", rec.date),
+                    ("check_in", "<=", end),
+                ]
+            )
+            domain = [("employee_id", "=", employee_id), ("date", "=", rec.date)]
+            if "holiday_id" in self._fields:
+                domain.append(("holiday_id", "=", None))
                 # si existe el campo holiday_id el cual es agregado por el modulo
                 # hr_holidays, si existe entonces se agrega al dominio para que no
                 # tenga en cuenta las lineas que son por ausencia
             total_time_register = self.search(domain)
-            total_time_register = sum(
-                total_time_register.mapped('unit_amount'))
-            current_worked_hours = sum(attendances.mapped(
-                'current_worked_hours'))
-            rec.unit_amount = (current_worked_hours - total_time_register)
+            total_time_register = sum(total_time_register.mapped("unit_amount"))
+            current_worked_hours = sum(attendances.mapped("current_worked_hours"))
+            rec.unit_amount = current_worked_hours - total_time_register
