bda540150a78e005cebdfccf4ac6bd5bf4b2077d
mav-adhoc <mav@adhoc.com.ar>
2026-06-19 18:42:06 +0000

    [ADD] website_hr_recruitment_ux: add Spanish translation for phone validation message
    Part-of: ingadhoc/hr#66
    Signed-off-by: Luciano Esperlazza <les@adhoc.inc>

--- website_hr_recruitment_ux/static/src/interactions/hr_recruitment_form.js
+++ website_hr_recruitment_ux/static/src/interactions/hr_recruitment_form.js
@@ -1,12 +1,17 @@
 import { HrRecruitmentForm } from "@website_hr_recruitment/interactions/hr_recruitment_form";
 import { patch } from "@web/core/utils/patch";
+import { _t } from "@web/core/l10n/translation";
 
 const MIN_PHONE_LENGTH = 7;
 
 patch(HrRecruitmentForm.prototype, {
     async checkRedundant(targetEl, field, messageContainerEl, keepPreviousWarningMessage = false) {
         if (field === "phone" && targetEl.value && targetEl.value.length < MIN_PHONE_LENGTH) {
-            this.hideWarningMessage(targetEl, messageContainerEl);
+            this.showWarningMessage(
+                targetEl,
+                messageContainerEl,
+                _t("Please enter a valid phone number.")
+            );
             return;
         }
         return super.checkRedundant(targetEl, field, messageContainerEl, keepPreviousWarningMessage);
